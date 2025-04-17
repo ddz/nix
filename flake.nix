083755a2fc5a67178e7ddb06f2efea02d1974929
Dino A. Dai Zovi <3083278+ddz@users.noreply.github.com>
2025-04-17 09:36:39 -0400

    Add initial flake from utils-generic template and gitignore

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Personal nix monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          git
+        ];
+      };
+    }
+  );
+}
